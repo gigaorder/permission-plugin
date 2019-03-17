f9c9a4bff4a08fe05d40571bb3e91007cdac26e6
Anh <anhoev@yahoo.de>
2019-03-17 23:35:30 +0700

    fix some bugs, optimize code

--- middleware/collection.js
+++ middleware/collection.js
@@ -3,12 +3,12 @@ const { getCollectionPermission, getHideFields, getQueryCondition } = require('.
 module.exports = cms => {
   return async function getCollectionMiddleware({ name, socket, collection }, next) {
     const permission = getCollectionPermission(socket.request.user, name);
-    const queryConditions = await getQueryCondition(socket.request.user, name);
+    const queryConditions = getQueryCondition(socket.request.user, name);
     if (!permission) {
       return next('error');
     }
     if (collection.info.alwaysLoad) {
-      const list = await cms.getModel(name).find(queryConditions || {});
+      const list = await cms.getModel(name).find((queryConditions && queryConditions.find) ? queryConditions.find : {});
       collection.list.push(...list);
     }
     const hideField = getHideFields(socket.request.user, name);
