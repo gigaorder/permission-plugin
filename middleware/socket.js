b033042d5699279eab0e1222a12567ea7714aa0d
ThinhVu <vutrongthinhk7@gmail.com>
2020-05-08 18:58:34 +0700

    Skip loginUrl path include query Correct route matching for nonAuthenticateUrls

--- middleware/socket.js
+++ middleware/socket.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const expireIn = 60 * 60 * 24 * 7; // 7 days
 const secretKey = 'eSdsL0AxK332GnqQbkWh';
+const url = require('url');
 const _ = require('lodash');
 
 module.exports = cms => {
@@ -9,12 +10,17 @@ module.exports = cms => {
       return next();
     }
     const referer = socket.request.headers.referer
-    if (referer && referer.endsWith(cms.data['loginUrl'])) {
-      if (!socket.handshake.query.token)
+    if (referer) {
+      const urlParts = url.parse(referer)
+      if (urlParts.pathname === cms.data['loginUrl']) {
+        if (!socket.handshake.query.token)
+          return next();
+      }
+      // using startsWith instead of === because referer path may be "path+params"
+      // e.g: /store/gigashop where as /store is path and gigashop is route params
+      if (cms.data['nonAuthenticateUrls'] && cms.data['nonAuthenticateUrls'].find(path => _.startsWith(urlParts.pathname, path))) {
         return next();
-    }
-    if (referer && cms.data['nonAuthenticateUrls'] && cms.data['nonAuthenticateUrls'].find(url => _.includes(referer, url))) {
-      return next();
+      }
     }
 
     let token = socket.handshake.query.token;
