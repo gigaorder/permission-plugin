31d9da13716818a5f627d00dc5071e0a1a92800a
danglam105 <danglam105@gmail.com>
2020-07-13 15:35:22 +0700

    Use Promise.all for parallel code

--- middleware/collection.js
+++ middleware/collection.js
@@ -4,36 +4,39 @@ const {getCollectionPermission, getHideFields, getQueryCondition} = require('./u
 module.exports = cms => {
   return async function getCollectionMiddleware({session, collections}, next) {
     let user = await cms.getModel('User').findById(session.userId);
+
     if (!user) {
       const role = await cms.getModel('Role').findOne({name: 'nouser'});
       if (role) user = {role};
     }
-    for (let collectionName in collections) {
+
+    await Promise.all(Object.keys(collections).map(async collectionName => {
       if (['ComponentBuilder', 'PluginFile'].includes(collectionName)) {
         collections[collectionName].list = await cms.getModel(collectionName).find({});
-        continue;
+        return;
       }
-      const collection = collections[collectionName];
+
       const permission = getCollectionPermission(user, collectionName);
       if (!permission) {
         delete collections[collectionName];
-        continue;
+        return;
       }
+
+      const collection = collections[collectionName];
       if (collection.info.alwaysLoad) {
-        const queryConditions = await getQueryCondition(user, collectionName);
-        if (queryConditions || user.role.name === 'admin') {
-          // collection.list = [];
-          const list = await cms.getModel(collectionName).find(queryConditions && queryConditions.find || {});
-          collection.list = list;
-        }
+          const queryConditions = await getQueryCondition(user, collectionName);
+          if (queryConditions || user.role.name === 'admin') {
+            collection.list = await cms.getModel(collectionName).find(queryConditions && queryConditions.find || {});
+          }
       }
-      const hideField = getHideFields(user, collectionName);
+
       // Remove hide field from form
+      const hideField = getHideFields(user, collectionName);
       if (Array.isArray(hideField)) {
         collection.form = collection.form.filter(item => !hideField.includes(item.key));
       }
-    }
-    next(null, {collections});
+    }));
 
+    next(null, {collections});
   };
 };
